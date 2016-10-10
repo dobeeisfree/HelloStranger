15c29bdd95f221aaba4980ac8c2359d6d3af8ab3
dobeeisfree <jubee0124@gmail.com>
2016-10-10 22:41:24 +0900

    회원가입시 타임픽커 배열 만듦
    헬퍼메소드 생성

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_filter :store_current_location, :unless => :devise_controller?
+  before_filter :select_time
 
   protected
   def store_current_location
@@ -13,4 +14,16 @@ class ApplicationController < ActionController::Base
    devise_parameter_sanitizer.permit(:sign_up,
    keys: [:name, :contact_number, :email, :password, :password_confirmation])
   end
+
+  # devise/registration/new
+  # 2016-10-10 , dobeeisfree
+  # 시간을 선택할 수 있도록 헬퍼메소드 만듦
+  def select_time
+    @time_picker = Array.new
+    24.times do |hour|
+       @time_picker.push("#{hour+1}:00")
+       @time_picker.push("#{hour+1}:30") unless hour+1 == 24
+    end
+  end
+
 end
