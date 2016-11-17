dd32371fccdae003ad57bf54142efb24fef7a168
dobeeisfree <jubee0124@gmail.com>
2016-11-18 03:53:55 +0900

    업로더가 모델에 마운트하기 위함
    실제 데이터베이스 스키마의 수정은
    `menus`테이블에있음
    구분하기 편하게 `food_picture`으로 바꿈

--- db/migrate/20160909183121_create_stores.rb
+++ db/migrate/20160909183121_create_stores.rb
@@ -10,7 +10,7 @@ class CreateStores < ActiveRecord::Migration
       t.string      :location
       t.string      :beacon_id
       t.string      :business_id
-			t.string      :main_picture
+			# t.string      :main_picture
 
       t.integer     :category , :default => 0
 
