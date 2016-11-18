1368db733eb55e2f836a65bab46c36515750e401
dobeeisfree <jubee0124@gmail.com>
2016-11-18 23:41:55 +0900

    매장현황 서브메뉴바 링크걸기 #10
    매장마다 또 달아줘야함 #10

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -1,9 +1,15 @@
 class BoardController < ApplicationController
+  before_action :set_stores
 
   def quick
   end
 
   def report
   end
+
+  private
+    def set_stores
+      @stores = Owner.find(current_owner.id).stores.all
+    end
 
 end
