ec4bb61959eef874fb4241fb3bd9980868b5409d
dobeeisfree <jubee0124@gmail.com>
2016-11-19 00:38:49 +0900

    매장당 각 메뉴별 퀵메뉴판을 위한 수정
    boolean값을 다 디폴트로 false!

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,6 +2,12 @@ class BoardController < ApplicationController
   before_action :set_stores
 
   def quick
+    if params[:store_id].to_s.blank?
+      puts "디폴트"
+      @menus = @stores.first.menus
+    else
+      @menus = @stores.find(params[:store_id]).menus
+    end
   end
 
   def report
