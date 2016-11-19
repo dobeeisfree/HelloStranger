afea60dc203e9a3580ebc77108dffa6bdee7f45c
dobeeisfree <jubee0124@gmail.com>
2016-11-19 20:00:39 +0900

    퀵메뉴판 2개로 제한

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -9,6 +9,18 @@ class BoardController < ApplicationController
       @menus = @stores.find(params[:store_id]).menu
     end
 
+    cnt = 0
+    @custom_err_msg = ""
+    @menus.each do |m|
+      if m.quick_menu == true
+        cnt = cnt + 1
+      end
+    end
+
+    if cnt == 2 && params[:set_quick]
+      @custom_err_msg = "현재 퀵메뉴판이 2개입니다."
+    end
+
     if params[:set_quick]
       @menu = @menus.find(params[:set_quick])
       @menu.set_quick(params[:set_quick])
