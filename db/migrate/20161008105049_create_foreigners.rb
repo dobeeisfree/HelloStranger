c50851a8bca45e155b173859f834f6fa450e6798
dobeeisfree <jubee0124@gmail.com>
2016-11-29 21:03:40 +0900

    유저로그인과 회원가입은 안드로이드에서 관리

--- db/migrate/20161008105049_create_foreigners.rb
+++ db/migrate/20161008105049_create_foreigners.rb
@@ -12,8 +12,6 @@ class CreateForeigners < ActiveRecord::Migration
       t.integer  :for_taboo, :default => 0
       # 0 ~ 3 (Kor, Eng, Jpn, Chin)
       t.integer  :lang, :null => false, :default => 0
-      # 로그인 후 서버에서 내려주는 토큰(임의의 문자열)은 API를 요청할 때 필요합니다.
-      t.string    :auth_token
     end
   end
 end
