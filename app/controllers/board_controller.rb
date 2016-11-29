683c576f71baef843e843f6f88f6fe62186fa59c
dobeeisfree <jubee0124@gmail.com>
2016-11-30 04:24:00 +0900

    매장현황에서 메뉴 삭제와 수정

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -2,32 +2,51 @@ class BoardController < ApplicationController
   before_action :set_stores
 
   def quick
-    if params[:store_id].to_s.blank?
-      puts "디폴트"
-      @menus = @stores.first.menu
-    else
-      @menus = @stores.find(params[:store_id]).menu
-    end
+
+    @menus = @stores.first.menu if params[:store_id].to_s.blank?
+    @menus = @stores.find(params[:store_id]).menu if params[:store_id]
 
     cnt = 0
-    @custom_err_msg = ""
+    @custom_err_msg = nil
     @menus.each do |m|
-      if m.quick_menu == true
-        cnt = cnt + 1
-      end
+      cnt = cnt + 1 if m.quick_menu == true
     end
 
-    if cnt == 2 && params[:set_quick]
-      @custom_err_msg = "현재 퀵메뉴판이 2개입니다."
-    end
+    @custom_err_msg = "현재 퀵메뉴판이 2개입니다." if cnt == 2 && params[:set_quick]
 
-    if params[:set_quick]
+
+    if params[:set_quick] && cnt <= 1
       @menu = @menus.find(params[:set_quick])
-      @menu.set_quick(params[:set_quick])
+      @menu.set_quick(params[:set_quick]) if @menu.quick_menu == false
     end
+
+    if cnt >= 2 && params[:set_quick]
+      @menu = @menus.find(params[:set_quick])
+      @menu.set_quick(params[:set_quick]) if @menu.quick_menu == true
+    end
+
   end
 
   def report
+
+    # TODO
+    @menus = @stores.first.menu if params[:store_id].to_s.blank?
+    @menus = @stores.find(params[:store_id]).menu if params[:store_id]
+
+    @max_menu = @menus.count
+
+    # 주문수 별
+    @cnt_jpn = @menus.sorted_by &:count_jpn
+    @cnt_chn = @menus.sorted_by &:count_chn
+    @cnt_kor = @menus.sorted_by &:count_kor
+    @cnt_eng = @menus.sorted_by &:count_eng
+
+    # 선호도 별
+    @jpn_like = @menus.sorted_by &:like_jpn
+    @chn_like = @menus.sorted_by &:like_chn
+    @kor_like = @menus.sorted_by &:like_kor
+    @eng_like = @menus.sorted_by &:like_eng
+
   end
 
   private
